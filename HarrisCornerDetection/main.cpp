adee4be2ed57a431b140896460b065aa8c26f8e7
SuperSix0 <34592509+SuperSix0@users.noreply.github.com>
2017-12-19 00:27:10 +0800

    实现Harris角点检测
    命令格式：“xxx.exe 图像文件 k参数(=0.04) Aperture_size = 3”
    显示中间的处理结果及最终的检测结果，包括最大特征值图，最小特征值图，R图，原图上叠加检测效果，并将中间结果输出成图像文件

--- HarrisCornerDetection/main.cpp
+++ HarrisCornerDetection/main.cpp
@@ -9,174 +9,11 @@
 using namespace cv;
 using namespace std;
 
-double* Gaussian;
-double k = 0.04;
-int k100;
-int apertureSize = 3;
-Mat picture, GrayPicture;
-string path, pic;
-int *Ix, *Iy;
-int *Ix2, *Iy2, *Ixy;
-double *A, *B, *C;
-int height, width;
-Mat maxPicture, minPicture, RPicture, NMSPicture, finalPicture, window;
-
-double* getGaussian(int n)
-{
-	double* dst;
-	double sum = 0, sigma = (n / 2) * 0.3 + 0.8;
-	dst = (double*)malloc(sizeof(double) * n * n);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			*(dst + i * n + j) = exp(-0.5 * (pow(i - n / 2, 2) + pow(j - n / 2, 2)) / pow(sigma, 2));
-			sum += *(dst + i * n + j);
-		}
-	}
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			*(dst + i * n + j) /= sum;
-	return dst;
-}
-
-void R()
-{
-	double a, b, c, max = 0;
-	double *tmp, temp;
-	tmp = (double*)malloc(sizeof(double) * height * width);
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			a = *(A + i * width + j);
-			b = *(B + i * width + j);
-			c = *(C + i * width + j);
-			temp = a * b - c * c - 0.01 * k100 * pow(a + b, 2);
-			*(tmp + i * width + j) = temp;
-			if (temp > max)
-				max = temp;
-		}
-	}
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			temp = *(tmp + i * width + j);
-			if(temp > max * 0.01)
-				RPicture.at<double>(i,j) = temp;
-			else
-				RPicture.at<double>(i, j) = 0;
-		}
-	}
-	imwrite(path + "\\R图_" + pic, RPicture);
-}
-
-void NMS()
-{
-	bool flag;
-	int row, col;
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			flag = true;
-			for (int subrow = -1; subrow <= 1; subrow++)
-			{
-				for (int subcol = -1; subcol <= 1; subcol++)
-				{
-					row = i + subrow;
-					col = j + subcol;
-					if (row < 0 || row >= height)
-						row = i;
-					if (col < 0 || col >= width)
-						col = j;
-					if (row == i && col == j)
-						continue;
-					if (RPicture.at<double>(i,j) < RPicture.at<double>(row, col))
-						flag = false;
-				}
-			}
-			if (flag)
-				NMSPicture.at<double>(i, j) = RPicture.at<double>(i, j);
-			else
-				NMSPicture.at<double>(i, j) = 0;
-		}
-	}
-	finalPicture = picture.clone();
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			if (NMSPicture.at<double>(i, j) > 0)
-				circle(finalPicture, Point(j, i), 3, Scalar(0, 0, 255));
-		}
-	}
-	imwrite(path + "\\原图叠加检测效果_" + pic, finalPicture);
-}
-
-Mat Merge(Mat mat1, Mat mat2, Mat mat3, Mat mat4)
-{
-	Mat dst(height * 2, width * 2, picture.type());
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			dst.at<unsigned char>(i, j * 3) = (unsigned char)mat1.at<double>(i, j);
-			dst.at<unsigned char>(i, j * 3 + 1) = (unsigned char)mat1.at<double>(i, j);
-			dst.at<unsigned char>(i, j * 3 + 2) = (unsigned char)mat1.at<double>(i, j);
-		}
-	}
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			dst.at<unsigned char>(i, (j + width) * 3) = (unsigned char)mat2.at<double>(i, j);
-			dst.at<unsigned char>(i, (j + width) * 3 + 1) = (unsigned char)mat2.at<double>(i, j);
-			dst.at<unsigned char>(i, (j + width) * 3 + 2) = (unsigned char)mat2.at<double>(i, j);
-		}
-	}
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			dst.at<unsigned char>(i + height, j * 3) = (unsigned char)mat3.at<double>(i, j);
-			dst.at<unsigned char>(i + height, j * 3 + 1) = (unsigned char)mat3.at<double>(i, j);
-			dst.at<unsigned char>(i + height, j * 3 + 2) = (unsigned char)mat3.at<double>(i, j);
-		}
-	}
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			dst.at<unsigned char>(i + height, (j + width) * 3) = (unsigned char)mat4.at<unsigned char>(i, j * 3);
-			dst.at<unsigned char>(i + height, (j + width) * 3 + 1) = (unsigned char)mat4.at<unsigned char>(i, j * 3 + 1);
-			dst.at<unsigned char>(i + height, (j + width) * 3 + 2) = (unsigned char)mat4.at<unsigned char>(i, j * 3 + 2);
-		}
-	}
-	return dst;
-}
-
-void on_Trackbar(int, void*)
-{
-	R();
-	NMS();
-	
-	double font_scale = 1;
-	int thickness = 2, baseline;
-	Size text_size = getTextSize("一", CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, thickness, &baseline);
-	putText(maxPicture, "Max", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
-	putText(minPicture, "Min", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
-	putText(RPicture, "R", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
-	putText(finalPicture, "Result", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
-	
-	window = Merge(maxPicture, minPicture, RPicture, finalPicture);
-	imshow("Harris Corner Detection", window);
-}
-
-
 int main(int argc, char** argv)
 {
+	string path, pic;
+	Mat picture;
+	
 	path = string(*(argv + 1));
 	picture = imread(path);
 	while (picture.empty())
@@ -188,11 +25,12 @@ int main(int argc, char** argv)
 
 	pic = path.substr(path.find_last_of("\\") + 1);
 	path = path.substr(0, path.find_last_of("\\"));
-
+	
+	double k = 0.04;
 	if (argc > 2)
 		k = atof(*(argv + 2));
-	k100 = k * 100;
 
+	int apertureSize = 3;
 	if (argc > 3)
 		apertureSize = atof(*(argv + 3));
 	while (apertureSize % 2 == 0 || apertureSize <= 1)
@@ -200,42 +38,49 @@ int main(int argc, char** argv)
 		cout << "apertureSize不符，请重新输入：\n";
 		cin >> apertureSize;
 	}
-
-	Gaussian = getGaussian(apertureSize);
-
+	
+	double *Gaussian;										//生成高斯二维分布函数
+	double sum = 0, sigma = (apertureSize / 2) * 0.3 + 0.8;
+	Gaussian = (double*)malloc(sizeof(double) * apertureSize * apertureSize);
+	for (int i = 0; i < apertureSize; i++)
+	{
+		for (int j = 0; j < apertureSize; j++)
+		{
+			*(Gaussian + i * apertureSize + j) = exp(-0.5 * (pow(i - apertureSize / 2, 2) + pow(j - apertureSize / 2, 2)) / pow(sigma, 2));
+			sum += *(Gaussian + i * apertureSize + j);
+		}
+	}
+	for (int i = 0; i < apertureSize; i++)
+		for (int j = 0; j < apertureSize; j++)
+			*(Gaussian + i * apertureSize + j) /= sum;
+	
+	Mat GrayPicture;
 	cvtColor(picture, GrayPicture, CV_RGB2GRAY);
 
-	height = picture.rows;
-	width = picture.cols;
-	Ix = (int *)malloc(sizeof(int) * height * width);
-	Iy = (int *)malloc(sizeof(int) * height * width);
-
-	int row, col;
+	int height = picture.rows;								//计算x、y方向偏导数
+	int width = picture.cols;
+	double *Ix, *Iy;
+	Ix = (double *)malloc(sizeof(double) * height * width);
+	Iy = (double *)malloc(sizeof(double) * height * width);
+	
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			row = i;
-			col = j;
-			int row1 = i - 1, row2 = i + 1;
-			int col1 = j - 1, col2 = j + 1;
-			if (row1 < 0)
-				row1 = 0;
-			if (row2 >= height)
-				row2 = height - 1;
-			if (col1 < 0)
-				col1 = 0;
-			if (col2 >= width)
-				col2 = width - 1;
+			int row1 = i > 0 ? (i - 1) : i;
+			int row2 = (i + 1) < height ? (i + 1) : i;
+			int col1 = j > 0 ? (j - 1) : j;
+			int col2 = (j + 1) < width ? (j + 1) : j;
 
-			*(Ix + i * width + j) = GrayPicture.data[row * width + col2] - GrayPicture.data[row * width + col1];
-			*(Iy + i * width + j) = GrayPicture.data[row2 * width + col] - GrayPicture.data[row1 * width + col];
+			*(Ix + i * width + j) = GrayPicture.at<unsigned char>(i, col2) - GrayPicture.at<unsigned char>(i, col1);
+			*(Iy + i * width + j) = GrayPicture.at<unsigned char>(row2, j) - GrayPicture.at<unsigned char>(row1, j);
 		}
 	}
-
-	Ix2 = (int *)malloc(sizeof(int) * height * width);
-	Iy2 = (int *)malloc(sizeof(int) * height * width);
-	Ixy = (int *)malloc(sizeof(int) * height * width);
+	
+	double *Ix2, *Iy2, *Ixy;								//计算Ix2、Iy2、Ixy
+	Ix2 = (double *)malloc(sizeof(double) * height * width);
+	Iy2 = (double *)malloc(sizeof(double) * height * width);
+	Ixy = (double *)malloc(sizeof(double) * height * width);
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
@@ -245,12 +90,13 @@ int main(int argc, char** argv)
 			*(Ixy + i * width + j) = *(Ix + i * width + j) * *(Iy + i * width + j);
 		}
 	}
-
-	double a, b, c;
+	
+	double a, b, c;											//将二维高斯分布函数作为加权函数计算A、B、C
+	int row, col;
+	double *A, *B, *C;
 	A = (double *)malloc(sizeof(double) * height * width);
 	B = (double *)malloc(sizeof(double) * height * width);
 	C = (double *)malloc(sizeof(double) * height * width);
-	apertureSize /= 2;
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
@@ -258,19 +104,19 @@ int main(int argc, char** argv)
 			a = 0;
 			b = 0;
 			c = 0;
-			for (int subrow = -apertureSize; subrow < apertureSize; subrow++)
+			for (int subrow = 0; subrow < apertureSize; subrow++)
 			{
-				for (int subcol = -apertureSize; subcol < apertureSize; subcol++)
+				for (int subcol = 0; subcol < apertureSize; subcol++)
 				{
-					row = i + subrow;
-					col = j + subcol;
+					row = i + subrow - apertureSize / 2;
+					col = j + subcol - apertureSize / 2;
 					if (row < 0 || row >= height)					//防止图像边界越界
 						row = i;
 					if (col < 0 || col >= width)
 						col = j;
-					a += *(Gaussian + (subrow + apertureSize) * width + subcol + apertureSize) * *(Ix2 + row * width + col);
-					b += *(Gaussian + (subrow + apertureSize) * width + subcol + apertureSize) * *(Iy2 + row * width + col);
-					c += *(Gaussian + (subrow + apertureSize) * width + subcol + apertureSize) * *(Ixy + row * width + col);
+					a += *(Gaussian + subrow * width + subcol) * *(Ix2 + row * width + col);
+					b += *(Gaussian + subrow * width + subcol) * *(Iy2 + row * width + col);
+					c += *(Gaussian + subrow * width + subcol) * *(Ixy + row * width + col);
 				}
 			}
 			*(A + i * width + j) = a;
@@ -279,15 +125,11 @@ int main(int argc, char** argv)
 		}
 	}
 
-	double max, min;
-	maxPicture.create(height, width, GrayPicture.type());
+	double max, min;											//计算最大特征值和最小特征值
+	Mat maxPicture(height, width, GrayPicture.type());
 	maxPicture.convertTo(maxPicture, CV_64F);
-	minPicture.create(height, width, GrayPicture.type());
+	Mat minPicture(height, width, GrayPicture.type());
 	minPicture.convertTo(minPicture, CV_64F);
-	RPicture.create(height, width, GrayPicture.type());
-	RPicture.convertTo(RPicture, CV_64F);
-	NMSPicture.create(height, width, GrayPicture.type());
-	NMSPicture.convertTo(NMSPicture, CV_64F);
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
@@ -305,10 +147,82 @@ int main(int argc, char** argv)
 	}
 	imwrite(path + "\\最大特征值图_" + pic, maxPicture);
 	imwrite(path + "\\最小特征值图_" + pic, minPicture);
+	
+	Mat RPicture(height, width, GrayPicture.type());
+	RPicture.convertTo(RPicture, CV_64F);
+	
+	max = 0;														//获得角点响应值R的最大值
+	double *tmp, temp;
+	tmp = (double*)malloc(sizeof(double) * height * width);
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			a = *(A + i * width + j);
+			b = *(B + i * width + j);
+			c = *(C + i * width + j);
+			temp = a * b - k * pow(a + b, 2);
+			*(tmp + i * width + j) = temp;
+			if (temp > max)
+				max = temp;
+		}
+	}
+	for (int i = 0; i < height; i++)								//设置阀值为最大值*0.01
+	{
+		for (int j = 0; j < width; j++)
+		{
+			temp = *(tmp + i * width + j);
+			if (temp > max * 0.01)
+				RPicture.at<double>(i, j) = temp;
+			else
+				RPicture.at<double>(i, j) = 0;
+		}
+	}
+	imwrite(path + "\\R图_" + pic, RPicture);
+	
+	Mat NMSPicture(height, width, GrayPicture.type());				//非极大值抑制
+	NMSPicture.convertTo(NMSPicture, CV_64F);
+	bool flag;
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			flag = true;
+			for (int subrow = -1; subrow <= 1; subrow++)
+			{
+				for (int subcol = -1; subcol <= 1; subcol++)
+				{
+					row = i + subrow;
+					col = j + subcol;
+					if (row < 0 || row >= height)
+						row = i;
+					if (col < 0 || col >= width)
+						col = j;
+					if (row == i && col == j)
+						continue;
+					if (RPicture.at<double>(i, j) < RPicture.at<double>(row, col))
+						flag = false;
+				}
+			}
+			if (flag)
+				NMSPicture.at<double>(i, j) = RPicture.at<double>(i, j);
+			else
+				NMSPicture.at<double>(i, j) = 0;
+		}
+	}
+	
+	Mat finalPicture = picture.clone();								//将角点在原图中用圆标注出来
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (NMSPicture.at<double>(i, j) > 0)
+				circle(finalPicture, Point(j, i), 3, Scalar(0, 0, 255));
+		}
+	}
+	imwrite(path + "\\原图叠加检测效果_" + pic, finalPicture);
 
-	R();
-	NMS();
-	namedWindow("Harris Corner Detection");
+	namedWindow("Harris Corner Detection");							//合并为一个图像并在窗口显示
 
 	double font_scale = 1;
 	int thickness = 2, baseline;
@@ -318,10 +232,46 @@ int main(int argc, char** argv)
 	putText(RPicture, "R", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
 	putText(finalPicture, "Result", Point(0, text_size.height), CV_FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, Scalar(255, 255, 255), thickness, 8, false);
 	
-	window = Merge(maxPicture,minPicture,RPicture,finalPicture);
+	Mat window(height * 2, width * 2, picture.type());
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			window.at<Vec3b>(i, j)[0] = (unsigned char)maxPicture.at<double>(i, j);
+			window.at<Vec3b>(i, j)[1] = (unsigned char)maxPicture.at<double>(i, j);
+			window.at<Vec3b>(i, j)[2] = (unsigned char)maxPicture.at<double>(i, j);
+		}
+	}
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			window.at<Vec3b>(i, j + width)[0] = (unsigned char)minPicture.at<double>(i, j);
+			window.at<Vec3b>(i, j + width)[1] = (unsigned char)minPicture.at<double>(i, j);
+			window.at<Vec3b>(i, j + width)[2] = (unsigned char)minPicture.at<double>(i, j);
+		}
+	}
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			window.at<Vec3b>(i + height, j)[0] = (unsigned char)RPicture.at<double>(i, j);
+			window.at<Vec3b>(i + height, j)[1] = (unsigned char)RPicture.at<double>(i, j);
+			window.at<Vec3b>(i + height, j)[2] = (unsigned char)RPicture.at<double>(i, j);
+		}
+	}
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			window.at<Vec3b>(i + height, j + width)[0] = finalPicture.at<Vec3b>(i, j)[0];
+			window.at<Vec3b>(i + height, j + width)[1] = finalPicture.at<Vec3b>(i, j)[1];
+			window.at<Vec3b>(i + height, j + width)[2] = finalPicture.at<Vec3b>(i, j)[2];
+		}
+	}
+	
 	imshow("Harris Corner Detection", window);
 
-	createTrackbar("k * 100", "Harris Corner Detection", &k100, 25, on_Trackbar);
 	waitKey(0);
 	return 0;
 }
